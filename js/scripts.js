4a7b86aa9f895d22580c15fe71f2408df3c04eb6
izz <izzatkhadim@gmail.com>
2020-12-02 15:08:36 -0500

    completed proj, make menu.html later

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+         $(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000, pause:"false" } );
+
+            $("#carouselButton").click(function(){
+                   $("#mycarousel").carousel('pause');
+                    if($('#carouselButton').children('span').hasClass('fa-pause')){
+                        $('#mycarousel').carousel('pause');
+                        $("#carouselButton").children('span').removeClass('fa-pause')
+                        $('#carouselButton').children('span').addClass('fa-play')
+                        alert("carousel is paused");
+                    }  
+                    else if($('#carouselButton').children('span').hasClass('fa-play')){
+                        $('#mycarousel').carousel('cycle');
+                        $('#carouselButton').children('span').removeClass('fa-play')
+                        $('#carouselButton').children('span').addClass('fa-pause')
+                        alert("carousel is resuming");
+                    }  
+                   
+               });
+
+         });
+         $(document).ready(function(){
+            $("#reservetableButton").click(function(){
+                $("#reservetableModal").modal('toggle')
+            });
+            
+         });
+
+         $(document).ready(function(){
+             $("#logintableButton").click(function(){
+                $("#loginModal").modal('toggle')
+             });
+         });
